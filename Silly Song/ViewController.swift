b32bd6fe16c7176505cc65f672f9abb3e809e176
Jeff Ripke <jripke74@gmail.com>
2016-01-27 15:28:04 -0800

    completed make connections.

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rest(sender: AnyObject) {
+        
+    }
 
+    @IBAction func displayLyrics(sender: AnyObject) {
+        
+    }
 }
 
