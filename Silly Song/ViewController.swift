8c1453bec49efd9504384d6d544d4148a066ceee
Jeff Ripke <jripke74@gmail.com>
2016-01-28 09:29:35 -0800

    Completed Silly Song Project

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,12 +24,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func rest(sender: AnyObject) {
-        
+    func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+        let shortName = shortNameFromName(fullName)
+        var lyricsFullName: String
+        var lyrics: String
+        lyricsFullName = lyricsTemplate.stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName)
+        lyrics = lyricsFullName.stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName)
+        return lyrics
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func shortNameFromName(fullName: String) -> String {
+        let lowercaseName = fullName.lowercaseString
+        let vowelSet = NSCharacterSet(charactersInString: "aeiou")
+        if let firstVowelRange = fullName.rangeOfCharacterFromSet(vowelSet, options: .CaseInsensitiveSearch) {
+            return lowercaseName.substringFromIndex(firstVowelRange.startIndex)
+        }
+        return lowercaseName
+    }
+    
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joinWithSeparator("\n")
+    
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+        nameField.returnKeyType = UIReturnKeyType.Done
+    }
+    
     @IBAction func displayLyrics(sender: AnyObject) {
-        
+        if let name = nameField.text?.capitalizedString {
+            lyricsView.text = lyricsForName(bananaFanaTemplate, fullName: name)
+        }
     }
 }
 
